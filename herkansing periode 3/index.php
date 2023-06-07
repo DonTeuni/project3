494c2a70adcd46ea2451582ff21f015f5cac2b29
DonTeuni <9018921@student.zadkine.nl>
2023-06-07 15:59:48 +0200

    update

--- herkansing periode 3/index.php
+++ herkansing periode 3/index.php
@@ -5,15 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <?php
     require_once '../herkansing periode 3/db.php';
     ?>
 
-    <class student>
-    <a href="../herkansing periode 3/student_toevoegen.php"><button>Student toevoegen</button></a>
-   <a href="../herkansing periode 3/studenten_latenzien.php"> <button> student laten zien</button></a>
-</class>
+    
+        <a href="../herkansing periode 3/student_toevoegen.php"><button class= "student-btn">Student toevoegen</button></a>
+        <a href="../herkansing periode 3/studenten_latenzien.php"> <button class= "student-btn2"> student laten zien</button></a>
+   
 </body>
 </html>
